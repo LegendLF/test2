d2892ac048587052842688f2b826d57d17cd48b4
Hun Zaw <hunz@synnex.com>
2023-10-31 10:24:05 -0700

    Initial commit of test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+import datetime
+
+# Get the current date and time
+current_datetime = datetime.datetime.now()
+
+# Extract and print the current date
+current_date = current_datetime.date()
+print("Current Date:", current_date)
